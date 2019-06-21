bf39d61fb1f40c75cb01c025595f51907d004853
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-06-21 08:12:23 +0100

    thursday update

--- app/controllers/journal_posts_controller.rb
+++ app/controllers/journal_posts_controller.rb
@@ -1,17 +1,44 @@
 class JournalPostsController < ApplicationController
 
     def index
-        journalPosts = Journal_post.all
+        journalPosts = JournalPost.all
         render json: journalPosts
     end
 
     def show
-        journalPost = Journalpost.find_by(id: params[:id])
+        journalPost = JournalPost.find_by(id: params[:id])
         if journalPost != nil
             render json: journalPost, except: [:user_id]
         else
             render json: { error: 'Journal post not found' }, status: 404
         end
     end
+
+    def create
+
+        journalPost = JournalPost.new(user_id: params[:user_id], description: params[:description])
+
+        if journalPost.save
+            journalPost.save
+            render json: journalPost
+        else
+            render json: {error: 'Journal post not saved'}, status: 400
+        end
+    end
+
+
+    def update 
+
+        journalPost = JournalPost.find_by(id: params[:id])
+        
+        journalPost.update(description: params[:description])
+
+        if journalPost.save 
+            
+            render json: journalPost
+        else
+            render json: { error: 'Couldnt update journal post' }, status: 404
+        end
+    end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :tasks, only: [:index, :show, :create, :destroy]
-  resources :journal_posts
+  resources :journal_posts, only: [:index, :show, :create, :update]
   resources :excuses
 end
